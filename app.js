2d90b3506b236056c4ce01ea8ff15fc5e56201b1
Subhodip11 <1941012209.subhodip@gmail.com>
2022-07-10 14:19:58 +0530

    solved middleware discrepency problem

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get('/worldData', (req, res) => {
     const payload = {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": process.env.API_KEy,
+            "x-rapidapi-key": process.env.API_KEY,
             "x-rapidapi-host": process.env.RAPID_API_HOST
         }
     };
